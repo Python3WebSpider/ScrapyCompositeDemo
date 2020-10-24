88dab09ad1192f8370af70f5b10d6d6f4b01316a
Germey <cqc@cuiqingcai.com>
2020-10-25 03:35:47 +0800

    uodate

---
+++ scrapycompositedemo/middlewares.py
@@ -0,0 +1,35 @@
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import aiohttp
+import logging
+
+
+class AuthorizationMiddleware(object):
+    accountpool_url = 'http://localhost:6789/antispider7/random'
+    logger = logging.getLogger('middlewares.authorization')
+
+    async def process_request(self, request, spider):
+        async with aiohttp.ClientSession() as client:
+            response = await client.get(self.accountpool_url)
+            if not response.status == 200:
+                return
+            credential = await response.text()
+            authorization = f'jwt {credential}'
+            self.logger.debug(f'set authorization {authorization}')
+            request.headers['authorization'] = authorization
+
+
+class ProxyMiddleware(object):
+    proxypool_url = 'http://localhost:5555/random'
+    logger = logging.getLogger('middlewares.proxy')
+
+    async def process_request(self, request, spider):
+        async with aiohttp.ClientSession() as client:
+            response = await client.get(self.proxypool_url)
+            if not response.status == 200:
+                return
+            proxy = await response.text()
+            self.logger.debug(f'set proxy {proxy}')
+            request.meta['proxy'] = f'http://{proxy}'
