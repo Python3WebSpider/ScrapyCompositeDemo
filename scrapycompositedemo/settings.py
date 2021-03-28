9e79eae03ea89629010d2f79135d86a9a84d9134
Germey <cqc@cuiqingcai.com>
2021-03-28 20:18:15 +0800

    update

--- scrapycompositedemo/settings.py
+++ scrapycompositedemo/settings.py
@@ -94,7 +94,7 @@ TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
-STATS_CLASS = "gerapy_redis.stats.RedisStatsCollector"
+STATS_CLASS = "scrapy_redis.stats.RedisStatsCollector"
 
 REDIS_URL = os.getenv('REDIS_URL')
 
